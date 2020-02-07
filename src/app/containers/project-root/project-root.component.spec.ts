15120a58de47b0d38e26653f789911ab735150f8
Phil Long <plong@onemedical.com>
2020-02-06 20:24:17 -0800

    Fix tests

--- src/app/containers/project-root/project-root.component.spec.ts
+++ src/app/containers/project-root/project-root.component.spec.ts
@@ -1,7 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { peopleStoreMockService } from '@app/__mocks__/people-store-mock.service';
+import { ResourceMockService } from '@app/__mocks__/resource-mock.service';
 import { ProjectNavComponent } from '@app/components/project-nav/project-nav.component';
+import { PeopleStoreService } from '@app/people-store.service';
+import { ProjectMembershipsService } from '@app/resources/project-memberships.service';
 
 import { ProjectRootComponent } from './project-root.component';
 
@@ -12,6 +16,13 @@ describe('ProjectRootComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ProjectRootComponent, ProjectNavComponent],
+      providers: [
+        {
+          provide: ProjectMembershipsService,
+          useValue: new ResourceMockService(),
+        },
+        { provide: PeopleStoreService, useValue: peopleStoreMockService },
+      ],
       imports: [RouterTestingModule],
     }).compileComponents();
   }));
